a0f52490e05dc39677278522527f8faff681a956
Prokop Ferdan <prokes1@gmail.com>
2024-07-19 15:29:12 +0200

    initializing repository

---
+++ src/main/java/com/prokys/demoSpringBootRESTCRUDAPIs/DemoSpringBootRestcrudapIsApplication.java
@@ -0,0 +1,13 @@
+package com.prokys.demoSpringBootRESTCRUDAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootRestcrudapIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootRestcrudapIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prokys/demoSpringBootRESTCRUDAPIs/DemoSpringBootRestcrudapIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokys.demoSpringBootRESTCRUDAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootRestcrudapIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
