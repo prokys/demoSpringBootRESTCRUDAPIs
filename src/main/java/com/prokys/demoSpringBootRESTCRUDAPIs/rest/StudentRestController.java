30a4ed7bd6d11a1d1eb4c4da0e74b48abb1bf9c6
Prokop Ferdan <prokes1@gmail.com>
2024-07-19 17:48:20 +0200

    created Student class, created StudentRestController for handling /api, added enpoint /students for returning list of students

---
+++ src/main/java/com/prokys/demoSpringBootRESTCRUDAPIs/rest/StudentRestController.java
@@ -0,0 +1,26 @@
+package com.prokys.demoSpringBootRESTCRUDAPIs.rest;
+
+import com.prokys.demoSpringBootRESTCRUDAPIs.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class StudentRestController {
+
+    //define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poornima", "Patell"));
+        theStudents.add(new Student("Mario", "Rosii"));
+        theStudents.add(new Student("Mary", "Smith"));
+
+        return theStudents;
+    }
+}
