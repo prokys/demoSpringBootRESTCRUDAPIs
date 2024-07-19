3747f8ca445a83bd3b1cf1ac68ebf483aed7c3f7
Prokop Ferdan <prokes1@gmail.com>
2024-07-19 19:14:44 +0200

    added @ExceptionHandler for any exception

--- src/main/java/com/prokys/demoSpringBootRESTCRUDAPIs/rest/StudentRestController.java
+++ src/main/java/com/prokys/demoSpringBootRESTCRUDAPIs/rest/StudentRestController.java
@@ -58,4 +58,20 @@ public class StudentRestController {
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    // add another exception handler for any exception
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+        //create a StudentErrorResponse
+
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
