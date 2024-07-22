d0bbb6654971c8d25f949fdce60a0c76116052ee
Prokop Ferdan <prokes1@gmail.com>
2024-07-22 09:47:48 +0200

    added StudentRestExceptionHandler for global exception handling, move code from StudentRestController

--- src/main/java/com/prokys/demoSpringBootRESTCRUDAPIs/rest/StudentRestController.java
+++ src/main/java/com/prokys/demoSpringBootRESTCRUDAPIs/rest/StudentRestController.java
@@ -2,8 +2,6 @@ package com.prokys.demoSpringBootRESTCRUDAPIs.rest;
 
 import com.prokys.demoSpringBootRESTCRUDAPIs.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -41,37 +39,5 @@ public class StudentRestController {
         }
 
         return theStudents.get(studentID);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        //create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler for any exception
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        //create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
